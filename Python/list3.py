a7f56177442401dfffb4132d2744ff868a689f31
Joseph Yong <jyong@MLSEA-JYONG01.local>
2015-12-28 10:29:33 -0800

    Finished

--- Python/list3.py
+++ Python/list3.py
@@ -5,5 +5,37 @@
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-  # +++your code here+++
-  return
+  rList = []
+  r2List = []
+  for i in tuples:
+    temp = i[::-1]
+    rList.append(temp)
+  rList.sort()
+  for j in rList:
+    temp = j[::-1]
+    r2List.append(temp)
+  return r2List
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+
+
+# Calls the above functions with interesting inputs.
+def main():
+  print 'sort_last'
+  test(sort_last([(1, 3), (3, 2), (2, 1)]),
+       [(2, 1), (3, 2), (1, 3)])
+  test(sort_last([(2, 3), (1, 2), (3, 1)]),
+       [(3, 1), (1, 2), (2, 3)])
+  test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+       [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+
+
+if __name__ == '__main__':
+  main()
