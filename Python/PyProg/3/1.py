61bc2d5b2dea7e7fc0f2670f85580db23a90b6ce
sangwon04 <yongj.spu@gmail.com>
2016-02-20 21:26:22 -0800

    Added 1

---
+++ Python/PyProg/3/1.py
@@ -0,0 +1,7 @@
+# Write a program to calculate the volume and surface area of a sphere from
+# its radius, given as input. Here are some formulas that might be useful:
+#                   V = 4/3(pi)r^3
+#                   A = 4(pi)r^2
+
+def main():
+    int radius = input("Please enter a radius: ")
