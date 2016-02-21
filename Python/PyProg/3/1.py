ac9879335b593baaf270fc0efc0e4c7649d36727
sangwon04 <yongj.spu@gmail.com>
2016-02-20 22:01:08 -0800

    Finished 1

--- Python/PyProg/3/1.py
+++ Python/PyProg/3/1.py
@@ -2,6 +2,16 @@
 # its radius, given as input. Here are some formulas that might be useful:
 #                   V = 4/3(pi)r^3
 #                   A = 4(pi)r^2
+import math
 
 def main():
-    int radius = input("Please enter a radius: ")
+    radius = input("Please enter a radius: ")
+    print "You entered " + str(radius) + " as your radius."
+
+    volume = round((4.0 / 3.0) * math.pi * radius ** 3, 2)
+    print "Given your radius, the volume of your sphere is " + str(volume) + "."
+
+    area = round(4 * math.pi * radius ** 2, 2)
+    print "Given your radius, the surface area of your sphere is " + str(area) + "."
+
+main()
