f203aa4948686efa7dd1ac3ce6b67260b17963f5
Joseph Yong <jyong@MLSEA-JYONG01.local>
2016-04-26 09:05:19 -0700

    blah

--- Python/PenT/netcat.py
+++ Python/PenT/netcat.py
@@ -1,3 +1,5 @@
+# UNFINISHED
+
 import sys
 import socket
 import getopt
@@ -5,3 +7,81 @@ import threading
 import subprocess
 
 # Define some global variables
+listen = False
+command = False
+upload = False
+execute = ""
+target = ""
+upload_destination = ""
+port = 0
+
+def usage():
+    print "BHP Net Tool"
+    print
+    print "Usage: bhpnet.py -t <target_host> -p <port>"
+    print "-l --listen                      - listen on [host]:[port] for incoming connections"
+    print "-e --execute=file-to-run         - execute the given file upon receiving a connection"
+    print "-c --command                     - initilize a command shell"
+    print "-u --upload=destination          - upon receiving connection upload a file and write" \
+                                              " to [destination]"
+    print
+    print
+    print "Examples: "
+    print "bhpnet.py -t 192.168.0.1 -p 5555 -l -c"
+    print "bhpnet.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe"
+    print "bhpnet.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\""
+    print "echo 'ABCDEFGHI' | ./bhpnet.py -t 192.168.0.1 -p 135"
+    sys.exit(0)
+
+def main():
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    # Read the commandline options
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hle:t:p:cu:",
+        ["help", "listen", "execute", "target", "port", "command", "upload"])
+    except getopt.GetoptError as err:
+        print str(err)
+        print usage()
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        elif o in ("-l", "--listen"):
+            listen = True
+        elif o in ("-c", "--commandshell"):
+            command = True
+        elif o in ("-u", "--upload"):
+            upload_destination = a
+        elif o in ("-t", "--target"):
+            target = a
+        elif o in ("-p", "--port"):
+            port = int(a)
+        else:
+            assert False, "Unhandled Option"
+
+    # Are we going to listen or just send data from stdin?
+    if not listen and len(target) and port > 0:
+        # Read in the buffer from the commandline
+        # This will block, so send CTRL+D if not sending input
+        # to stdin
+        buffer = sys.stdin.read()
+
+        # Send data off
+        client_sender(buffer)
+
+    # We are going to listen and potentially upload things,
+    # execute commands, and drop a shell back depending on
+    # our command line options above
+    if listen:
+        sever_loop()
+
+main()
