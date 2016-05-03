2aade0fe81b5792a3ee41032ce04e7c8df4c4895
sangwon04 <yongj.spu@gmail.com>
2016-05-02 23:27:54 -0700

    Update

--- Python/vt_cache.py
+++ Python/vt_cache.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 
 import argparse
+import sys
 
 def main(args):
-    print args
+    if args.action == 'clear':
+        print 'Clearing cache for %s' % args.hash
+        sys.exit()
+    else:
+        print 'Looking up hash, %s, in cache...' % args.hash
+        sys.exit()
 
 if __name__ == "__main__":
     argParser = argparse.ArgumentParser()
-    argParser.add_argument("-s", "--sha256", help = 'This is the SHA256 hash you wish to lookup', required = False)
-    argParser.add_argument("-m", "--md5", help = 'This is the MD5 has you wish to lookup', required = False)
+    argParser.add_argument("-a", "--action", choices = ['clear', 'lookup'], help = 'Determines to either \
+                           clear the cache or lookup the current cache', required = True)
+    argParser.add_argument("-f", "--hash", help = 'This is the hash you wish to lookup', required = True)
     main(argParser.parse_args())
