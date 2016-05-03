deb83e3a76605b8dbd244801b18755373c439f44
Joseph Yong <jyong@MLSEA-JYONG01.lan>
2016-05-02 22:48:20 -0700

    add

--- Python/vt_cache.py
+++ Python/vt_cache.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
-import sys
 
-print sys.argv[0]
+import argparse
+
+def main(args):
+    print args
+
+if __name__ == "__main__":
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("-a", "--first", required = True)
+    argParser.add_argument("-b", "--second", required = False)
+    main(argParser.parse_args())
