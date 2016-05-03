15cbd170ce85046ab421be52675519550640cd3a
sangwon04 <yongj.spu@gmail.com>
2016-05-02 23:03:37 -0700

    chmod +x

--- Python/vt_cache.py
+++ Python/vt_cache.py
@@ -2,11 +2,14 @@
 
 import argparse
 
+def usage():
+    print 'usage: vt_cache.py '
+
 def main(args):
     print args
 
 if __name__ == "__main__":
     argParser = argparse.ArgumentParser()
-    argParser.add_argument("-a", "--first", required = True)
-    argParser.add_argument("-b", "--second", required = False)
+    argParser.add_argument("-a", "--first", help = 'help me', required = True)
+    argParser.add_argument("-b", "--second", help = 'help me, too', required = False)
     main(argParser.parse_args())
