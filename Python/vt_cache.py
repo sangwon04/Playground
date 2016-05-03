78e97ddb18aebf92ca477d83e9a351d76829249e
sangwon04 <yongj.spu@gmail.com>
2016-05-02 23:13:50 -0700

    test

--- Python/vt_cache.py
+++ Python/vt_cache.py
@@ -7,6 +7,6 @@ def main(args):
 
 if __name__ == "__main__":
     argParser = argparse.ArgumentParser()
-    argParser.add_argument("-a", "--first", help = 'help me', required = True)
-    argParser.add_argument("-b", "--second", help = 'help me, too', required = False)
+    argParser.add_argument("-s", "--sha256", help = 'This is the SHA256 hash you wish to lookup', required = False)
+    argParser.add_argument("-m", "--md5", help = 'This is the MD5 has you wish to lookup', required = False)
     main(argParser.parse_args())
