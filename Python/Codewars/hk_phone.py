17332e9f350e220bb10a78ae4ea2ae9ba195d3d4
sangwon04 <yongj.spu@gmail.com>
2016-05-18 19:30:17 -0700

    blah

--- Python/Codewars/hk_phone.py
+++ Python/Codewars/hk_phone.py
@@ -17,8 +17,10 @@ Task
 Define two functions, isValidHKPhoneNumber and hasValidHKPhoneNumber, that returns whether a given string is a valid HK phone number
 and contains a valid HK phone number respectively (i.e. true/false values).
 '''
+import re
+
 def isValidHKPhoneNumber(number):
-    if not number or number[0] == " " or len(number) <= 7 or number[4] != " ":
+    if not number or number[0] == " ":
         return False
     else:
         if number[5] == " ":
@@ -26,6 +28,8 @@ def isValidHKPhoneNumber(number):
         else:
             number = number.split()
             for x in number:
+                if len(x) != 4:
+                    return False
                 try:
                     int(x)
                 except ValueError:
@@ -33,15 +37,39 @@ def isValidHKPhoneNumber(number):
             return True
 
 def hasValidHKPhoneNumber(number):
-    if len(number.split()) <= 2:
+    hkNumRegex = re.compile(r'\d\d\d\d \d\d\d\d')
+    match = hkNumRegex.search(number)
+    if match:
+        return True
+    else:
+        return False
+
+
+    num_list = []
+    if len(number.split()) < 2:
         return False
     else:
+        number = number.split()
         for x in number:
-            
-    return False
+            try:
+                int(x)
+                num_list.append(x)
+            except ValueError:
+                continue
+        if len(num_list) > 0:
+            for numbers in num_list:
+                try:
+                    int(numbers)
+                    if len(numbers) != 4:
+                        return False
+                except ValueError:
+                    return False
+            return True
+        else:
+            return False
 
 def main():
-    if hasValidHKPhoneNumber('abc 1234 5678 def'):
+    if isValidHKPhoneNumber("8400 84999"):
         print "True"
     else:
         print "False"
