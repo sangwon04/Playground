d35ad75a934546c361284d8dbad909e59de55a62
Joseph Yong <jyong@MLSEA-JYONG01.local>
2016-06-24 15:02:30 -0700

    Requests

--- Python/authSig.py
+++ Python/authSig.py
@@ -7,21 +7,48 @@ import hashlib
 import hmac
 import base64
 import email.utils
+import requests
+import logging
 
-# uuid = raw_input('UUID: ')
-# uuid.encode('utf-8')
-sKey = raw_input('API Secret Key: ')
+# Grab the UUID
+# uuid = bytes(raw_input('UUID: '))
+uuid = 'cff6e27c-bf01-42ba-a4aa-94abcbf81501'
+uuid.encode('utf-8')
+
+# Grab the API key
+#sKey = bytes(raw_input('API Secret Key: '))
+sKey = 'NmUzZTU5ZjctNGM2NC00ZDI1LWEzYjctMTBlNmNlOTAyNjNm'
 sKey.encode('utf-8')
+
+# Sign the request w/ API key
 method = 'GET'
 content_md5 = base64.b64encode(hashlib.md5("").digest())
 date = email.utils.formatdate(localtime=False, usegmt=True)
 path = "firehose.crowdstrike.com/sensors/entities/datafeed/v1"
 query_string = "?appId=joseph"
+requestString = "\n".join([method, date, path, query_string])
+signature = base64.b64encode(hmac.new(sKey, content_md5, requestString, hashlib.sha256).digest())
+print requestString + '\n'
+print "Signature: %r\n\n" % signature
 
-requestString = "\n".join([method, content_md5, date, path, query_string])
-signature = base64.b64encode(hmac.new(sKey, requestString, hashlib.sha256).digest())
-print "Signature: %r" % signature
+# These two lines enable debugging at httplib level (requests->urllib3->http.client)
+# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# The only thing missing will be the response.body which is not logged.
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
 
-# string_to_sign = "\n".join([method, content_md5, date, path, query_string])
-# dig = hmac.new(auth.api_key, string_to_sign, hashlib.sha256).digest()
-# return base64.b64encode(dig).decode()
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
+# Send the Request
+headers = {'X-CS-Date': date, 'Authorization': '%s %s:%s:customers' % ('cs-hmac', uuid, signature) }
+r = requests.get('https://' + path + query_string, headers = headers)
+print r.status_code
